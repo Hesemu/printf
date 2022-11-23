ee783a8fe7c719a6ee4f14be3dc41cd3491044bf
Zeelgabriels <zealgabrles@gmail.com>
2022-11-22 16:07:36 -0800

    Task_0.Review

--- _printf.c
+++ _printf.c
@@ -1,56 +1,68 @@
-#include <stdarg.h>
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- *_printf-Own Printf function
- *@format: Int Value
- *
- *Return: Int value
+ * _printf - Personal Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int i = 0, j = 0, counter = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-        format_opt data_t[] = {
-                {"c", print_c}, {"s", print_s}, {"%", print_mod},
-                {"i", print_i},
-                {"d", print_d}, {"R", rot_13}, {"r", print_rev},
-                {"u", print_deci},
-                {"b", print_binary}, {"o", print_octal},
-                {"x", print_hexa},
-                {"X", print_hexa_UP}, {NULL, NULL}
-        };
-        va_start(args, format);
-        if (format == NULL)
-                return (-1);
-        for (i = 0; format && format[i]; i++)
-        {
-                if (format[i] != '%')
-                {
-                        if (_putchar(format[i]) < 0)
-                                return (-1);
-                        counter++;
-                        continue;
-                }
-                if (format[i + 1] == '\0')
-                        return (-1);
-                for (j = 0; data_t[j].type; j++)
-                {
-                        if (*data_t[j].type == format[i + 1])
-                        {
-                                counter += data_t[j].f(args);
-                                break;
-                        }
-                }
-                if (data_t[j].type == NULL)
-                {
-                        if (_putchar(format[i]) < 0 || _putchar(format[i + 1])
-                                        < 0)
-                                return (-1);
-                        counter += 2; }
-                i++;
-        }
-        va_end(args);
-        return (counter);
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Would print the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
