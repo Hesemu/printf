1fc05213e49fd5d61a04a459d52ff5967782c1f3
Herbert <esemuherbert4@gmail.com>
2022-11-24 06:17:10 -0800

    functions1.c review

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include MAIN_H
+#include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
 
--- functions1.c
+++ functions1.c
@@ -1,4 +1,4 @@
-#include MAIN_H
+#include "main.h"
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
